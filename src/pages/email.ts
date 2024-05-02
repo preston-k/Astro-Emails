7c642ce94161177f67080e027d2a454f6413f04c
Preston <71202878+preston-k@users.noreply.github.com>
2024-05-02 10:32:11 -0700

    Emails

---
+++ src/pages/email.ts
@@ -0,0 +1,30 @@
+import type { APIContext } from 'astro'
+import { createTransport } from "nodemailer"
+
+const config = {
+  host: import.meta.env.NODEMAIL_HOST,
+  port: +import.meta.env.NODEMAIL_PORT,
+  secure: import.meta.env.NODEMAIL_PORT == "465",
+  auth: {
+    user: import.meta.env.NODEMAIL_USER,
+    pass: import.meta.env.NODEMAIL_PASSWORD
+  }
+}
+
+const transport = createTransport(config)
+
+export async function POST(context: APIContext) {
+  const data = await context.request.formData()
+  const sendto = data.get("sendto")?.toString()
+  const subject = data.get("subject")?.toString()
+  const content = data.get("content")?.toString()
+  await transport.sendMail({
+    subject: subject,
+    text: content,
+    to: sendto,
+    from: "noreply@prestonkwei.com"
+  })
+
+  return new Response("hi")
+}
+
