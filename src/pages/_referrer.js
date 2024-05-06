fe74ed774288454a73ac458017947c4c454c2563
Preston <71202878+preston-k@users.noreply.github.com>
2024-05-06 11:16:02 -0700

    Email format

--- src/pages/_referrer.js
+++ src/pages/_referrer.js
@@ -186,8 +186,8 @@ let code = generateCode()
 const data = new FormData()
 data.set('sendto', sendEmail)
 data.set('subject','A New Login Has Been Detected')
-data.set('html','<h1>Hello</h1>')
-data.set('content', `Hi! A new login has been detected in your account. Please enter code: ${await code}`)
+data.set('html','<h1>Hello!</h1><p>A new login has been detected in your account. Please enter code: ${await code}</p>')
+data.set('content', `Hello! A new login has been detected in your account. Please enter code: ${await code}`)
 fetch("/email", {
   method: "POST",
   body:data,
