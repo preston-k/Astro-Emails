c8325f2249599cb2873bdc2476d11ae4b555fa61
Preston <71202878+preston-k@users.noreply.github.com>
2024-05-06 11:14:23 -0700

    EMAIL Address Change

--- src/pages/_referrer.js
+++ src/pages/_referrer.js
@@ -180,9 +180,11 @@ async function generateCode() {
   }) 
   return code
 }
+
+let sendEmail = email.replace(/,/g, '.').replace(/_/g, '@')
 let code = generateCode()
 const data = new FormData()
-data.set('sendto', email)
+data.set('sendto', sendEmail)
 data.set('subject','A New Login Has Been Detected')
 data.set('html','<h1>Hello</h1>')
 data.set('content', `Hi! A new login has been detected in your account. Please enter code: ${await code}`)
