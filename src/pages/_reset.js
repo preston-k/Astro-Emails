074e005016754161cab81f94ce8f87f024c6b179
Preston <71202878+preston-k@users.noreply.github.com>
2024-05-07 14:11:56 -0700

    Email Fix

--- src/pages/_reset.js
+++ src/pages/_reset.js
@@ -37,8 +37,8 @@ let resetform = document.querySelector('#emailinputform').addEventListener('subm
   const data = new FormData()
   data.set('sendto', email)
   data.set('subject','Here\'s the link to reset your password')
-  data.set('html',`<h1>Hello!</h1><p>We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: https://emailserver.prestonkwei.com/reset?now=true&id=${uuid()}&e=${firebaseemail}.</p>`)
-  data.set('content', `We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: https://emailserver.prestonkwei.com/reset?now=true&id=${uuid()}&e=${firebaseemail}.`) // FALLBACK IN CASE THE HTML FAILS
+  data.set('html',`<h1>Hello!</h1><p>We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: https://emailserver.prestonkwei.com/reset?now=true&id=${uuid()}&e=${email}.</p>`)
+  data.set('content', `We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: https://emailserver.prestonkwei.com/reset?now=true&id=${uuid()}&e=${email}.`) // FALLBACK IN CASE THE HTML FAILS
   fetch("/email", {
     method: "post",
     body:data,
