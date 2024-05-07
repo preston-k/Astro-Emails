6d2c9ceac91211a8aaed05ddeb701f121ed08267
Preston <71202878+preston-k@users.noreply.github.com>
2024-05-07 11:10:26 -0700

    No Captcha

--- src/pages/_reset.js
+++ src/pages/_reset.js
@@ -12,7 +12,7 @@ let resetform = document.querySelector('#emailinputform').addEventListener('subm
   data.set('sendto', email)
   data.set('subject','Here\'s the link to reset your password')
   data.set('html',`<h1>Hello!</h1><p>We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: <a href='https://3400659c-5937-4c08-b29b-33fb25dac89a.prestonkwei.com/reset?now=true&id=${uuid()}.`)
-  data.set('content', `We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: <a href='https://3400659c-5937-4c08-b29b-33fb25dac89a.prestonkwei.com/reset?now=true&id=${uuid()}.`)
+  data.set('content', `We just received a request to reset your password. If you did NOT request this link, please disreguard this email.</p><p>Click this link or copy and paste it into your browser to reset your password: <a href='https://3400659c-5937-4c08-b29b-33fb25dac89a.prestonkwei.com/reset?now=true&id=${uuid()}.`) // FALLBACK IN CASE THE HTML FAILS
   fetch("/email", {
     method: "POST",
     body:data,
