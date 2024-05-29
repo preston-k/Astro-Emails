c7f0c5e9a6fd66e7ca96ca2d4de11cb59fb56886
Preston Kwei <prestonkwei@gmail.com>
2024-05-28 21:39:43 -0700

    Dizzy Animation Stopper

--- src/pages/_referrer.js
+++ src/pages/_referrer.js
@@ -1,26 +1,26 @@
-import "https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js";
-import "https://www.gstatic.com/firebasejs/8.0.0/firebase-database.js";
-// import "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"
-import { SnackBar } from "./snackbar";
+import 'https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js';
+import 'https://www.gstatic.com/firebasejs/8.0.0/firebase-database.js';
+// import 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js'
+import { SnackBar } from './snackbar';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-ZYqrpT04a5zOkB5uQYK3lE3CuMhkhC8",
-  authDomain: "oauth-page-ad3c2.firebaseapp.com",
-  databaseURL: "https://oauth-page-ad3c2-default-rtdb.firebaseio.com",
-  projectId: "oauth-page-ad3c2",
-  storageBucket: "oauth-page-ad3c2.appspot.com",
-  messagingSenderId: "401481049573",
-  appId: "1:401481049573:web:f1f9ca852e96d580cf3b0c",
+  apiKey: 'AIzaSyB-ZYqrpT04a5zOkB5uQYK3lE3CuMhkhC8',
+  authDomain: 'oauth-page-ad3c2.firebaseapp.com',
+  databaseURL: 'https://oauth-page-ad3c2-default-rtdb.firebaseio.com',
+  projectId: 'oauth-page-ad3c2',
+  storageBucket: 'oauth-page-ad3c2.appspot.com',
+  messagingSenderId: '401481049573',
+  appId: '1:401481049573:web:f1f9ca852e96d580cf3b0c',
 };
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database();
 function readCookie(cookieName) {
-  const nameEQ = cookieName + "=";
-  const cookiesArray = document.cookie.split(";");
+  const nameEQ = cookieName + '=';
+  const cookiesArray = document.cookie.split(';');
   let cd = null;
   for (let i = 0; i < cookiesArray.length; i++) {
     let cookie = cookiesArray[i];
-    while (cookie.charAt(0) === " ") {
+    while (cookie.charAt(0) === ' ') {
       cookie = cookie.substring(1, cookie.length);
     }
     if (cookie.indexOf(nameEQ) === 0) {
@@ -31,112 +31,115 @@ function readCookie(cookieName) {
   ``;
   return cd;
 }
-let disableAnimation = document.querySelector("#disable");
+let disableAnimation = document.querySelector('#disable');
 
-disableAnimation.addEventListener("click", () => {
-  document.querySelector("#loader").styles.display = "none";
+disableAnimation.addEventListener('click', () => {
+  console.log('Disable Animation')
+  document.querySelector('#verify-loader').style.animation = 'none'
+  document.querySelector('#verify-loader').style.background = 'lightblue'
+  console.log(document.querySelector('#verify-loader'))
 });
 // check for snackbars here
 function cannot() {
-  document.querySelector("#missingperms").style.display = "block";
-  document.querySelector("#sucess").style.display = "none";
-  document.querySelector("#error").style.display = "none";
+  document.querySelector('#missingperms').style.display = 'block';
+  document.querySelector('#sucess').style.display = 'none';
+  document.querySelector('#error').style.display = 'none';
   // document.body.classList.add('greens-screen-animation')
-  document.querySelector("#maincontent").style.display = "none";
+  document.querySelector('#maincontent').style.display = 'none';
 }
 const urlParams = new URLSearchParams(window.location.search);
-let email = urlParams.get("e");
+let email = urlParams.get('e');
 console.log(email);
-let what = urlParams.get("do");
+let what = urlParams.get('do');
 console.log(what);
-let ts = urlParams.get("ts");
+let ts = urlParams.get('ts');
 console.log(ts);
-let id = urlParams.get("id");
+let id = urlParams.get('id');
 console.log(id);
-if (email == "" || email == null) {
+if (email == '' || email == null) {
   cannot();
 }
-if (ts == "" || email == null) {
+if (ts == '' || email == null) {
   cannot();
 }
-if (id == "" || email == null) {
+if (id == '' || email == null) {
   cannot();
 }
-if (what == "" || email == null) {
+if (what == '' || email == null) {
   cannot();
 }
 async function twofactor() {
-  let id = urlParams.get("id");
-  let inputtedcode = document.querySelector("#num").value;
+  let id = urlParams.get('id');
+  let inputtedcode = document.querySelector('#num').value;
   console.log(inputtedcode);
-  document.getElementById("maincontent").style.display = "none";
-  document.getElementById("sucess").style.display = "none";
-  document.getElementById("error").style.display = "none";
-  document.getElementById("verify").style.display = "block";
+  document.getElementById('maincontent').style.display = 'none';
+  document.getElementById('sucess').style.display = 'none';
+  document.getElementById('error').style.display = 'none';
+  document.getElementById('verify').style.display = 'block';
   firebase
     .database()
     .ref(`2fa/${inputtedcode}`)
-    .once("value")
+    .once('value')
     .then((snapshot) => {
       const data = snapshot.val();
       if (data) {
         const { id, email, status } = data;
         console.log(id);
-        if (status == "unused") {
-          database.ref("2fa/" + inputtedcode).update({
-            status: "used",
+        if (status == 'unused') {
+          database.ref('2fa/' + inputtedcode).update({
+            status: 'used',
           });
-          document.getElementById("maincontent").style.display = "none";
-          document.getElementById("verify").style.display = "none";
-          document.getElementById("error").style.display = "none";
-          document.getElementById("sucess").style.display = "block";
-          document.body.classList.add("green-screen-animation");
+          document.getElementById('maincontent').style.display = 'none';
+          document.getElementById('verify').style.display = 'none';
+          document.getElementById('error').style.display = 'none';
+          document.getElementById('sucess').style.display = 'block';
+          document.body.classList.add('green-screen-animation');
           let queryString = window.location.search.substring(1);
           console.log(queryString);
           setTimeout(() => {
             window.location.replace(
-              "https://oauth.prestonkwei.com/account?" +
+              'https://oauth.prestonkwei.com/account?' +
                 queryString +
-                "&immediate=authtoken"
+                '&immediate=authtoken'
             );
           }, 4000);
         } else {
-          document.getElementById("maincontent").style.display = "none";
-          document.getElementById("sucess").style.display = "none";
-          document.getElementById("verify").style.display = "none";
-          document.getElementById("error").style.display = "block";
-          document.body.classList.add("red-screen-animation");
+          document.getElementById('maincontent').style.display = 'none';
+          document.getElementById('sucess').style.display = 'none';
+          document.getElementById('verify').style.display = 'none';
+          document.getElementById('error').style.display = 'block';
+          document.body.classList.add('red-screen-animation');
           setTimeout(() => {
             window.location.replace(
-              "https://oauth.prestonkwei.com/login?f2fa=true"
+              'https://oauth.prestonkwei.com/login?f2fa=true'
             );
           }, 4000);
         }
       } else {
         // Can't find auth token
-        document.getElementById("maincontent").style.display = "none";
-        document.getElementById("sucess").style.display = "none";
-        document.getElementById("verify").style.display = "none";
-        document.getElementById("error").style.display = "block";
-        document.body.classList.add("red-screen-animation");
+        document.getElementById('maincontent').style.display = 'none';
+        document.getElementById('sucess').style.display = 'none';
+        document.getElementById('verify').style.display = 'none';
+        document.getElementById('error').style.display = 'block';
+        document.body.classList.add('red-screen-animation');
         setTimeout(() => {
           window.location.replace(
-            "https://oauth.prestonkwei.com/login?f2fa=true"
+            'https://oauth.prestonkwei.com/login?f2fa=true'
           );
         }, 5000);
       }
     })
     .catch((error) => {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     });
 }
 
-if (email == null || email == "") {
+if (email == null || email == '') {
   cannot();
 }
 async function generateCode() {
-  let code = "";
-  const numbers = "0123456789";
+  let code = '';
+  const numbers = '0123456789';
   while (code.length < 6) {
     const digit = numbers[Math.floor(Math.random() * numbers.length)];
     if (!code.includes(digit)) {
@@ -146,69 +149,69 @@ async function generateCode() {
   let d = new Date();
   let ts = d.toString();
 
-  await database.ref("2fa/" + code).update({
+  await database.ref('2fa/' + code).update({
     code: code,
     email: email,
-    status: "unused",
+    status: 'unused',
     ts: ts,
   });
   return code;
 }
 
-let sendEmail = email.replace(/,/g, ".").replace(/_/g, "@");
+let sendEmail = email.replace(/,/g, '.').replace(/_/g, '@');
 let hiddenemail = sendEmail.replace(
   /^(..).*(.)(@.)(.*)(..)(\..*)$/,
-  "$1***$2$3$4**$5$6"
+  '$1***$2$3$4**$5$6'
 );
 document.querySelector(
-  "#sentto"
+  '#sentto'
 ).innerHTML = `We just sent an email containing a 6 digit code to ${hiddenemail}. Please enter it below to finish logging into your account!`;
 
-if (sessionStorage.getItem("emailsent") != "true") {
+if (sessionStorage.getItem('emailsent') != 'true') {
   let code = generateCode();
   const data = new FormData();
-  data.set("sendto", sendEmail);
-  data.set("subject", "Here's the login code you requested");
+  data.set('sendto', sendEmail);
+  data.set('subject', 'Here\'s the login code you requested');
   data.set(
-    "html",
+    'html',
     `<style>.main {border: 2px solid black;}<div id='main'></style><h1>Hello!</h1><p>A new login has been detected in your account. To protect your account, please use this code provided below. <div id='sub'>Please enter code: <strong>${await code}</strong></p><p>If you didn't request this code...</p><p>Change your password IMMEDIATELY. Somebody has your password, but don\'t worry, they do not have access to your account. We blocked their access. Reset your password here: https://emailserver.prestonkwei.com/reset</div></div>`
   );
   data.set(
-    "content",
+    'content',
     `Hello! A new login has been detected in your account. Please enter code: ${await code}. If you didn't request this code...</p><p>Change your password IMMEDIATELY. Somebody has your password, but don\'t worry, they do not have access to your account. We blocked their access. Reset your password here: https://emailserver.prestonkwei.com/reset`
   );
-  fetch("/email", {
-    method: "POST",
+  fetch('/email', {
+    method: 'POST',
     body: data,
   }).catch(() => {});
   let now = new Date();
-  let time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+  let time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
   sessionStorage.clear();
-  sessionStorage.setItem("emailsent", "true");
-  sessionStorage.setItem("emailsent-ts", time);
-  console.log("Email sent");
-  if (urlParams.get("snackbar") == "sent") {
+  sessionStorage.setItem('emailsent', 'true');
+  sessionStorage.setItem('emailsent-ts', time);
+  console.log('Email sent');
+  if (urlParams.get('snackbar') == 'sent') {
     SnackBar({
-      position: "tc",
-      message: "Email Sent Sucessfully!",
-      status: "green",
-      width: "80vw",
+      position: 'tc',
+      message: 'Email Sent Sucessfully!',
+      status: 'green',
+      width: '80vw',
     });
   }
 }
-document.querySelector("#num").addEventListener("input", () => {
-  if (document.querySelector("#num").value.length == 6) {
+document.querySelector('#num').addEventListener('input', () => {
+  if (document.querySelector('#num').value.length == 6) {
     twofactor();
   }
 });
-document.querySelector("#sendnew").addEventListener("click", () => {
-  console.log("Send new code");
-  let sendtime = sessionStorage.getItem("emailsent-ts");
+document.querySelector('#sendnew').addEventListener('click', () => {
+  console.log('Send new code');
+  let sendtime = sessionStorage.getItem('emailsent-ts');
   let curNow = new Date();
   let curTime =
-    curNow.getHours() + ":" + curNow.getMinutes() + ":" + curNow.getSeconds();
+    curNow.getHours() + ':' + curNow.getMinutes() + ':' + curNow.getSeconds();
   function timeToSeconds(time) {
-    let [hours, minutes, seconds] = time.split(":");
+    let [hours, minutes, seconds] = time.split(':');
     return Number(hours) * 3600 + Number(minutes) * 60 + Number(seconds);
   }
 
@@ -218,17 +221,17 @@ document.querySelector("#sendnew").addEventListener("click", () => {
 
   if (timeDifference > 60) {
     sessionStorage.clear();
-    console.log("More than 60 seconds have passed.");
-    window.location.replace(window.location.href + "&snackbar=sent");
+    console.log('More than 60 seconds have passed.');
+    window.location.replace(window.location.href + '&snackbar=sent');
   } else {
     console.log(`${60 - timeDifference} seconds more.`);
     SnackBar({
-      position: "tc",
+      position: 'tc',
       message: `Please wait ${
         60 - timeDifference
       } seconds before sending another email!`,
-      status: "red",
-      width: "80vw",
+      status: 'red',
+      width: '80vw',
     });
   }
 });
