4979e1c7ab3fd6ef70050967d2ba47fc48ecf617
Preston Kwei <prestonkwei@gmail.com>
2024-07-24 19:39:21 -0700

    Cors Headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  server: {
+    headers: {
+      "access-control-allow-origin": "*",
+      "access-control-allow-methods": "POST",
+      "access-control-allow-headers": "x-requested-with, content-type, authorization"
+      },
+  }
 });
