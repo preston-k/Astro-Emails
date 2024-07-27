b87e0fff3372cb5395762e725e1f3da01da1728a
Preston Kwei <prestonkwei@gmail.com>
2024-07-26 17:10:30 -0700

    Logging

--- src/pages/email.ts
+++ src/pages/email.ts
@@ -14,17 +14,21 @@ const config = {
 const transport = createTransport(config)
 
 export async function POST(context: APIContext) {
+  console.log("1. received request")
   try {
     const data = await context.request.formData()
     const sendto = data.get('sendto')?.toString()
     const subject = data.get('subject')?.toString()
     const content = data.get('content')?.toString()
     const html = data.get('html')?.toString()
+    console.log("2. got form data successfully")
 
     if (!sendto || !subject || !content) {
+      console.log("3a. fields are missing")
       return new Response('Missing required fields', { status: 400 })
     }
 
+    console.log("3b. sending...")
     await transport.sendMail({
       subject,
       text: content,
@@ -32,10 +36,14 @@ export async function POST(context: APIContext) {
       from: 'noreply@prestonkwei.com',
       html
     })
+    console.log("4b. sent email!")
 
     return new Response('Email sent successfully!', { status: 200 })
   } catch (error) {
+    console.log("4c. didn't send!")
     console.error('Error sending email:', error)
     return new Response('Internal Server Error', { status: 500 })
+  } finally {
+    console.log("5. executed finally branch")
   }
 }
