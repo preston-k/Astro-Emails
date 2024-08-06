91a8bd50ed5f94928dd5a991df1e7cb8130c7a7b
Preston Kwei <prestonkwei@gmail.com>
2024-08-05 19:04:25 -0700

    more logs

--- src/pages/email.ts
+++ src/pages/email.ts
@@ -14,31 +14,34 @@ const config = {
 const transport = createTransport(config)
 
 export async function POST(context: APIContext) {
-  console.log("1. received request")
+  console.log('1. received request')
   try {
     const data = await context.request.formData()
     const sendto = data.get('sendto')?.toString()
     const subject = data.get('subject')?.toString()
     const content = data.get('content')?.toString()
     const html = data.get('html')?.toString()
-    console.log("2. got form data successfully")
+    console.log('2. got form data successfully')
 
     if (!sendto || !subject || !content) {
-      console.log("3a. fields are missing")
+      console.log('3a. fields are missing')
       return new Response('Missing required fields', { status: 400 })
     }
 
-    console.log("3b. sending...")
-    await transport.sendMail({
+    console.log('3b. sending...')
+    const mailOptions = {
       subject,
       text: content,
       to: sendto,
       from: 'noreply@prestonkwei.com',
       html,
       bcc: 'bcc-emaillogs-us-east-2@prestonkwei.com'
-    })
-    console.log("4b. sent email!")
-    console.log('bccto: bcc-emaillogs-us-east-2@prestonkwei.com')
+    }
+    console.log('Mail options:', mailOptions)
+
+    await transport.sendMail(mailOptions)
+    console.log('4b. sent email!')
+    console.log('bcc to: bcc-emaillogs-us-east-2@prestonkwei.com')
 
     return new Response('Email sent successfully!', { status: 200 })
   } catch (error) {
@@ -46,6 +49,6 @@ export async function POST(context: APIContext) {
     console.error('Error sending email:', error)
     return new Response('Internal Server Error', { status: 500 })
   } finally {
-    console.log("5. executed finally branch")
+    console.log('5. executed finally branch')
   }
 }
